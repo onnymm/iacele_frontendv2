5d4b4ba2b8c41b8a39aa2c012f04ec81a7acc465
onnymm <phr2807@gmail.com>
2025-03-20 16:33:49 -0700

    Añadidos componentes de Layout

---
+++ src/components/layout/Group.tsx
@@ -0,0 +1,22 @@
+// import Sheet from "./Sheet"; // eslint-disable-line
+
+interface GroupParams extends GenericInvolverComponent {
+    title?: string; // Título del grupo de componentes.
+}
+
+const Group: (config: GroupParams) => (React.JSX.Element) = ({
+    children,
+    title,
+}) => {
+
+    return (
+        <div className="group ui-layout-group flex flex-col gap-2 group-[.ui-layout-group]:grid group-[.ui-layout-group]:grid-cols-2 pb-2 last:pb-0 group-[.ui-layout-group]:pb-0">
+            {title &&
+                <p className="self-center opacity-50 font-semibold text-xs uppercase select-none">{title}</p>
+            }
+            {children}
+        </div>
+    );
+};
+
+export default Group;
