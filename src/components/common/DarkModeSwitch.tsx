6675a2c730c47f74cd98cfaf50f8ef0bffded101
onnymm <phr2807@gmail.com>
2025-03-21 17:33:16 -0700

    Creado componente de switch de modo oscuro

---
+++ src/components/common/DarkModeSwitch.tsx
@@ -0,0 +1,43 @@
+import { Switch } from "@heroui/react"
+import { useContext } from "react"
+import DarkModeContext from "../../contexts/darkModeContext"
+import IconMoon from "../icons/IconMoon";
+import IconSun from "../icons/IconSun";
+
+/** 
+ *  ## Switch de modo oscuro
+ *  Este componente renderiza un swtich que activa o desactiva el modo oscuro
+ *  de la aplicación
+ *  
+ *  `< tsx />` Se autocierra.
+ *  
+ *  ### Parámetros de entrada
+ *  Este componente no requiere parámetros de entrada.
+ */ 
+const DarkModeSwitch = (): (React.JSX.Element) => {
+
+    // Obtención de valores del contexto
+    const {darkMode, setDarkMode} = useContext<IACele.Context.DarkMode>(DarkModeContext);
+
+    return (
+        <Switch classNames={{wrapper: 'shadow-[inset_0px_1px_2px_0px_rgba(0,_0,_0,_0.3)]'}} size="sm" isSelected={darkMode} onValueChange={setDarkMode} thumbIcon={<ThumbIcon isSelected={darkMode} />} />
+    );
+};
+
+export default DarkModeSwitch;
+
+const ThumbIcon: React.FC<{isSelected: boolean}> = ({
+    isSelected,
+}) => {
+
+    // Ícono a renderizar
+    const IconToRender = (
+        isSelected
+            ? IconMoon
+            : IconSun
+    );
+
+    return (
+        <IconToRender className="fill-gray-500" />
+    )
+}
---
+++ src/components/icons/IconMoon.tsx
@@ -0,0 +1,15 @@
+const IconMoon: (config: React.SVGProps<SVGSVGElement>) => React.JSX.Element = ({
+    className,
+}) => {
+
+    console.log(className);
+    
+
+    return (
+        <svg className={className} viewBox="0 -960 960 960">
+            <path d="M483-172q-128.33 0-218.17-89.83Q175-351.67 175-480q0-113 71.5-197.5T425-783q-14 28-22 59t-8 64q0 111.67 78.17 189.83Q551.33-392 663-392q33 0 64-8t58-22q-20 107-104.5 178.5T483-172Z"/>
+        </svg>
+    );
+};
+
+export default IconMoon;
