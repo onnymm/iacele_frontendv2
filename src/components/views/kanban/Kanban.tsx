399bb36f13a1d959efa6f5b77e559444c9ad948b
onnymm <phr2807@gmail.com>
2025-04-15 16:49:07 -0700

    Añadida funcionalidad para abrir detalle de registro en formulario

--- src/components/views/kanban/Kanban.tsx
+++ src/components/views/kanban/Kanban.tsx
@@ -1,3 +1,7 @@
+import { useContext } from "react";
+import useOpenRecord from "../../../hooks/views/useOpenRecord";
+import RecordKanbanContext from "../../../contexts/recordKanbanContext";
+
 /** 
  *  ## Kanban
  *  Este componente renderiza la tarjeta base que envuelve campos y secciones
@@ -12,9 +16,15 @@ const Kanban: React.FC<IACele.UI.GenericInvolverComponent> = ({
     children,
 }) => {
 
+    // Obtención de parámetros de redireccionamiento para abrir detalle de registro
+    const { recordPath, openRecord } = useOpenRecord();
+    // Obtención del registro para extraer su ID
+    const { record } = useContext(RecordKanbanContext);
+
     return (
         <div
-            className="flex flex-col gap-2 bg-white hover:bg-slate-100 dark:bg-[#1f2f3f] shadow-md dark:hover:brightness-125 p-4 border border-gray-500/20 cursor-pointer"
+            onClick={() => openRecord(record.id)}
+            className={`${recordPath ? 'hover:bg-slate-100 dark:hover:brightness-125 cursor-pointer' : ''} flex flex-col gap-2 bg-white dark:bg-[#1f2f3f] shadow-md p-4 border border-gray-500/20`}
         >
             {children}
         </div>
